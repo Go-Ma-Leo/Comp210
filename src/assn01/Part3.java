5c39a46baa0d25e89958ccbe0ebdc8485aaf0a45
toolaototalk <mosken@ad.unc.edu>
2023-01-22 12:42:27 -0500

    hw

--- src/assn01/Part3.java
+++ src/assn01/Part3.java
@@ -8,12 +8,10 @@ public class Part3 {
     }
     public static void method2(){
         Integer n2 = 0xABC;
-        int num1 = Integer.parseInt(n2.toString(),16);
-        System.out.println(num1);
+        System.out.println(n2);
     }
     public static void method3(){
-        String[] a3 = {"a", "z"};
+        char[] a3 = new char[]{'a', 'z'};
         System.out.println(a3[0]+""+a3[1]);
-
     }
 }
