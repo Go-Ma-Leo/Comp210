76a1beee9643b2b74fbd90cac5d14e140586987f
toolaototalk <mosken@ad.unc.edu>
2023-01-23 14:40:42 -0500

    hw

--- src/assn01/Part3.java
+++ src/assn01/Part3.java
@@ -4,14 +4,15 @@ public class Part3 {
     public static void main(String args[]){
         short sh = 32767;
         method2();
-        method3();
+
     }
     public static void method2(){
         Integer n2 = 0xABC;
         System.out.println(n2);
+        method3();
     }
     public static void method3(){
-        char[] a3 = new char[]{'a', 'z'};
-        System.out.println(a3[0]+""+a3[1]);
+        int[] a3 = new int[]{'a', 'z'};
+        System.out.println(a3[0]+" "+a3[1]);
     }
 }
